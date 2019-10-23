604c61318279656ea05cc44520ee67dd0cb17847
philoteer <cdac1234@gmail.com>
2019-10-23 20:36:56 +0900

    Update hfont_to_kotex_config.bat

--- hfont_to_kotex_config.bat
+++ hfont_to_kotex_config.bat
@@ -1,4 +1,4 @@
- powershell -Command "iex \"& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI\""
+powershell -Command "iex \"& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI\""
 echo powershell -Command "(get-content %%~n1%%~x1) -replace '(usepackage{.*)hfont', '$1kotex' | Out-File -encoding UTF8NoBOM \"%%~n1_kotex%%~x1\"" > hfont_to_kotex.bat
 
 copy hfont_to_kotex.bat "%USERPROFILE%\SendTo"
