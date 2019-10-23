8b1b3debe00769804149858ce0acb7ee73e829f4
philoteer <cdac1234@gmail.com>
2019-10-23 21:08:29 +0900

    Update hfont_to_kotex_config.bat

--- hfont_to_kotex_config.bat
+++ hfont_to_kotex_config.bat
@@ -1,6 +1,7 @@
 rem Requires Powershell 6
 
-echo powershell -Command "(get-content %%~n1%%~x1) -replace '(usepackage{.*)hfont', '$1kotex' | Out-File -encoding UTF8NoBOM \"%%~n1_kotex%%~x1\"" > hfont_to_kotex.bat
+echo "C:\Program Files\PowerShell\6\pwsh.exe" -Command "(get-content %%~n1%%~x1) -replace '(usepackage{.*)hfont', '$1kotex' | Out-File -encoding UTF8NoBOM \"%%~n1_kotex%%~x1\"" > hfont_to_kotex.bat
+rem echo powershell -Command "(get-content %%~n1%%~x1) -replace '(usepackage{.*)hfont', '$1kotex' | Out-File -encoding UTF8NoBOM \"%%~n1_kotex%%~x1\"" > hfont_to_kotex.bat
 
 copy hfont_to_kotex.bat "%USERPROFILE%\SendTo"
 del hfont_to_kotex.bat
