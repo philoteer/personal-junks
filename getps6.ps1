0c43b06c43b41c2d3bf92a6ec3daaf0a8b83cc10
philoteer <cdac1234@gmail.com>
2019-10-23 21:16:18 +0900

    Update getps6.ps1

--- getps6.ps1
+++ getps6.ps1
@@ -1,1 +1,4 @@
 iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
+
+# x86_64 msi: https://app.box.com/s/1in50pe04w9qa90zvcgwwgirg22eaya9
+# x86 msi: https://app.box.com/s/662a1s1tu3fzh92btx9zzh871ialmm20
