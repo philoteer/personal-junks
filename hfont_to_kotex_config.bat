e129f85ecf533c642709149d1bfb9a4dbb403f4c
philoteer <cdac1234@gmail.com>
2019-10-23 18:33:45 +0900

    Update hfont_to_kotex_config.bat

--- hfont_to_kotex_config.bat
+++ hfont_to_kotex_config.bat
@@ -1,5 +1,3 @@
-echo powershell -Command "(get-content %%1) -replace 'hfont', 'kotex' | Out-File -encoding utf8 \"%%~n1_kotex%%~x1\"" > hfont_to_kotex.bat
+echo powershell -Command "(get-content %%1) -replace '(usepackage{.*)hfont', '$1kotex' | Out-File -encoding utf8 \"%%~n1_kotex%%~x1\"" > hfont_to_kotex.bat
 
 copy hfont_to_kotex.bat "%USERPROFILE%\SendTo"
-
-set /p Input=Enter Yes or No:
