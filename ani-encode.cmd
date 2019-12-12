b7b911b0fef2806bfaa0159ada8d8ff0049b3c63
philoteer <cdac1234@gmail.com>
2019-12-12 12:06:41 +0900

    Update ani-encode.cmd

--- ani-encode.cmd
+++ ani-encode.cmd
@@ -3,7 +3,7 @@ if "%~1" == "" goto done
 
 md "%~p1/enc"
 
-C:\ffmpeg\ffmpeg.exe -y -i "%~1" -threads 0 -sn -vcodec libx264 -preset slower -crf 25 -tune animation -sws_flags lanczos -acodec aac -strict experimental -ac 2 -ab 128k -r 23.98 "%~p1/enc/%~n1.mp4"
+C:\ffmpeg\ffmpeg.exe -y -i "%~1" -threads 0 -sn -vcodec libx264 -preset slower -crf 25 -tune animation -sws_flags lanczos -vf "scale=w=min(iw\,1280):h=-2" -acodec aac -strict experimental -ac 2 -ab 128k -r 23.98 "%~p1/enc/%~n1.mp4"
 
 shift
 goto again
