d115130db315fd2c74f28a8459c4b354d4b0777c
philoteer <cdac1234@gmail.com>
2020-03-03 02:45:25 +0900

    Update oneshot_pdf.sh

--- oneshot_lossy_compression/oneshot_pdf.sh
+++ oneshot_lossy_compression/oneshot_pdf.sh
@@ -8,7 +8,7 @@ find . -name "*.pdf" > pdf_list
 echo "Verifying the functionality of shrinkpdf script."
 first_file=$(head -n 1 pdf_list)
 cp "$first_file" 1.pdf 
-shrinkpdf 1.pdf 2.pdf || { echo 'something is wrong' ; exit 1; }
+./pdfshrink.sh 1.pdf 2.pdf || { echo 'something is wrong' ; exit 1; }
 rm 1.pdf 2.pdf
 
 echo "shrinkpdf is working; proceeding to the actual conversion stage."
@@ -20,7 +20,7 @@ do
 	echo "$j"
 	mv "$j" 1.pdf
 	#shrinkpdf 1.pdf 2.pdf || { echo 'something is wrong' ; exit 1; }
-  shrinkpdf 1.pdf 2.pdf
+	./pdfshrink.sh 1.pdf 2.pdf
 	rm 1.pdf
 	mv 2.pdf "$j"
 done
