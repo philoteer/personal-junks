84fc471558ccfccd4128ac57b7e02e0de8195dbf
philoteer <cdac1234@gmail.com>
2020-03-04 04:13:26 +0900

    Update oneshot_pdf.sh

--- oneshot_lossy_compression/oneshot_pdf.sh
+++ oneshot_lossy_compression/oneshot_pdf.sh
@@ -5,18 +5,18 @@
 #list of variables
 TMP_FILENAME_1=__1.pdf
 TMP_FILENAME_2=__2.pdf
-TMP_LIST_FILE_NAME=__pdf_list
+TMP_LIST_FILENAME=__pdf_list
 
 DPI=150
 SHRINKPDF_PATH="pdfshrink.sh"
 
 #get pdf list
 echo "Creating a file list."
-find . -iname "*.pdf" > "$TMP_LIST_FILE_NAME"
+find . -iname "*.pdf" > "$TMP_LIST_FILENAME"
 
 #shrinkpdf functionality check
 echo "Verifying the functionality of shrinkpdf script."
-first_file=$(head -n 1 "$TMP_LIST_FILE_NAME")
+first_file=$(head -n 1 "$TMP_LIST_FILENAME")
 cp "$first_file" "$TMP_FILENAME_1" 
 sh "$SHRINKPDF_PATH" "$TMP_FILENAME_1"  "$TMP_FILENAME_2" || { echo 'something is wrong' ; exit 1; }
 rm "$TMP_FILENAME_1"  "$TMP_FILENAME_2"
@@ -25,7 +25,7 @@ echo "shrinkpdf is working; proceeding to the actual conversion stage."
 
 #Do the actual task.
 IFS=$'\n'
-for j in $(cat "./$TMP_LIST_FILE_NAME")
+for j in $(cat "./$TMP_LIST_FILENAME")
 do
 	echo "$j"
 	cp "$j" "$TMP_FILENAME_1"  #used to be mv instead; changed to minimize chance of losing files. 
@@ -37,4 +37,4 @@ do
 done
 
 #remove the tmp file.
-rm "$TMP_LIST_FILE_NAME"
+rm "$TMP_LIST_FILENAME"
