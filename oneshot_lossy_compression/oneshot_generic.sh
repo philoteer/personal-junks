178a87210b2fedf21011a930f38beb7dd230bf2a
philoteer <cdac1234@gmail.com>
2020-03-06 19:13:34 +0900

    Create oneshot_generic.sh

---
+++ oneshot_lossy_compression/oneshot_generic.sh
@@ -0,0 +1,45 @@
+#!/bin/bash	
+
+#(for now, some bashisms are going on)
+
+#list of variables
+#EXTENSION=pdf
+#EXEC_PATH="pdfshrink.sh"
+#QUAL_ARG=150
+
+TMP_FILENAME_1="__1.$EXTENSION"
+TMP_FILENAME_2="__2.$EXTENSION"
+TMP_LIST_FILENAME="__$EXTENSION_list"
+
+#get file list
+echo "Creating a file list."
+find . -iname "*.$EXTENSION" > "$TMP_LIST_FILENAME"
+LINES_COUNT=$(wc -l "$TMP_LIST_FILENAME" | awk '{print $1}')
+
+#exec functionality check
+echo "Verifying the functionality of the converter program."
+first_file=$(head -n 1 "$TMP_LIST_FILENAME")
+cp "$first_file" "$TMP_FILENAME_1" 
+sh "$EXEC_PATH" "$TMP_FILENAME_1"  "$TMP_FILENAME_2" || { echo 'something is wrong' ; exit 1; }
+rm "$TMP_FILENAME_1"  "$TMP_FILENAME_2"
+
+echo "converter is working; proceeding to the actual conversion stage."
+
+#Do the actual task.
+IFS=$'\n'
+CURRENT_COUNT=1
+for j in $(cat "./$TMP_LIST_FILENAME")
+do
+	echo "[$CURRENT_COUNT/$LINES_COUNT] $j"
+	cp "$j" "$TMP_FILENAME_1"  #used to be mv instead; changed to minimize chance of losing files. 
+	#sh $EXEC_PATH "$TMP_FILENAME_1" "$TMP_FILENAME_2"|| { echo 'something is wrong' ; exit 1; }
+	sh "$EXEC_PATH" "$TMP_FILENAME_1"  "$TMP_FILENAME_2" $QUAL_ARG
+	rm "$TMP_FILENAME_1" 
+	rm "$j"
+	mv "$TMP_FILENAME_2" "$j"
+	
+	CURRENT_COUNT=`expr $CURRENT_COUNT + 1`
+done
+
+#remove the tmp file.
+rm "$TMP_LIST_FILENAME"
