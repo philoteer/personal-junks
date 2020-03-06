9385a56db0ce97d41573aed4f9b68a91665fecce
philoteer <cdac1234@gmail.com>
2020-03-06 19:48:39 +0900

    Update oneshot_generic.sh

--- oneshot_lossy_compression/oneshot_generic.sh
+++ oneshot_lossy_compression/oneshot_generic.sh
@@ -3,10 +3,6 @@
 #(for now, some bashisms are going on)
 
 #list of variables
-#EXTENSION=pdf
-#TARGET_EXTENSION=pdf
-#EXEC_PATH="pdfshrink.sh"
-#QUAL_ARG=150
 
 TMP_FILENAME_1="__1.${EXTENSION}"	#original file (temporarily copied)
 TMP_FILENAME_2="__2.${TARGET_EXTENSION}"	#output file
