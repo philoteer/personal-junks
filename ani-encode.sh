949bfcbc16585746f3550c126d7e3d008a29bfef
philoteer <cdac1234@gmail.com>
2022-08-21 17:25:11 +0900

    Update ani-encode.sh

--- ani-encode.sh
+++ ani-encode.sh
@@ -12,6 +12,6 @@ do
 	mkdir -p "${dir}/enc"
 	#$FFMPEG_BIN -y -i "$F" -threads 0 -sn -vcodec libx264 -preset slower -crf 25 -tune animation -sws_flags lanczos -vf "scale=w=min(iw\,1280):h=-2" -acodec aac -strict experimental -ac 2 -ab 128k -r 23.98 "${dir}/enc/${filename_noext}.mp4"
 	
-	$FFMPEG_BIN -y -i "$F" -threads 0 -sn -c:v libaom-av1 -cpu-used 4 -row-mt true -crf 30 -tile-columns 1 -tile-rows 1 -sws_flags lanczos -vf "scale=w=min(iw\,1280):h=-2" -acodec aac -strict experimental -ac 2 -ab 128k -r 23.98 "${dir}/enc/${filename_noext}.mp4"
+	$FFMPEG_BIN -y -i "$F" -threads 0 -sn -c:v libaom-av1 -cpu-used 4 -row-mt true -crf 30 -tile-columns 1 -tile-rows 1 -sws_flags lanczos -vf "scale=w=min(iw\,1280):h=-2" -c:a libopus -strict normal -ac 2 -ab 128k -r 23.98 "${dir}/enc/${filename_noext}.mp4"
 	
 done
