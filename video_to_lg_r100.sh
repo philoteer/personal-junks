d3d00cda49e88d57177f29eba9bcecde3b02ee35
philoteer <ksshin@postech.ac.kr>
2025-01-11 06:11:46 +0900

    Update video_to_lg_r100.sh

--- video_to_lg_r100.sh
+++ video_to_lg_r100.sh
@@ -5,12 +5,8 @@ ENCODER="libx264"
 #vaapi does not work (not sure how to fix this); nvenc works.
 
 #stretch
-#ffmpeg -i "$1" -filter:v "transpose=1,scale=960:1080,setsar=1" -codec:v $ENCODER -f matroska -y _left.mkv
-#ffmpeg -i "$1" -filter:v "transpose=2,scale=960:1080,setsar=1" -s 960x1080 -codec:v $ENCODER -f matroska -y _right.mkv
-
-#letterbox
-ffmpeg -i "$1" -filter:v "transpose=1,scale=960:1080," -codec:v $ENCODER -f matroska -y _left.mkv
-ffmpeg -i "$1" -filter:v "transpose=2,scale=960:1080," -s 960x1080 -codec:v $ENCODER -f matroska -y _right.mkv
+ffmpeg -i "$1" -filter:v "transpose=1,scale=960:1080,setsar=1" -codec:v $ENCODER -f matroska -y _left.mkv
+ffmpeg -i "$1" -filter:v "transpose=2,scale=960:1080,setsar=1" -s 960x1080 -codec:v $ENCODER -f matroska -y _right.mkv
 
 ffmpeg -i _left.mkv -i _right.mkv -filter_complex hstack -codec:v $ENCODER "$1_R100.mkv"
 rm _left.mkv
