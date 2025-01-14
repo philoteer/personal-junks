01f1792fa8746db1face7dc5aa4f68fa0b8b6b92
philoteer <ksshin@postech.ac.kr>
2025-01-15 02:53:59 +0900

    Update video_to_lg_r100.sh

--- video_to_lg_r100.sh
+++ video_to_lg_r100.sh
@@ -2,15 +2,15 @@
 # original source: https://nurdspace.nl/LG_R100_info_collection
 
 ENCODER="libx264"
-#ENCODER="h264_nvenc"
-#vaapi does not work (not sure how to fix this)
+#vaapi does not work (not sure how to fix this); nvenc works.
 
 #stretch
-ffmpeg -i "$1" -filter:v "transpose=1,scale=640:720,setsar=1" -codec:v $ENCODER -f matroska -y _left.mkv
-ffmpeg -i "$1" -filter:v "transpose=2,scale=640:720,setsar=1" -codec:v $ENCODER -f matroska -y _right.mkv
+ffmpeg -i "$1" -filter:v "transpose=1,scale=608:1080,setsar=1,pad=860:1230:0:75:black" -codec:v $ENCODER -f matroska -y _left.mkv
+ffmpeg -i "$1" -filter:v "transpose=2,scale=608:1080,setsar=1,pad=608:1230:0:75:black" -codec:v $ENCODER -f matroska -y _right.mkv
 
-ffmpeg -i _left.mkv -i _right.mkv -filter_complex hstack -codec:v $ENCODER "$1_R100.mkv"
+ffmpeg -i _left.mkv -i _right.mkv -filter_complex "hstack" -codec:v $ENCODER "$1_R100.mkv"
 rm _left.mkv
 rm _right.mkv
 
 #playback: mplayer -monitorpixelaspect 1.5 rollercoaster-LGR100.mp4
+ 
