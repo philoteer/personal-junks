2711819eeb97effdfd22743ce1fdcf7aaabf2516
philoteer <ksshin@postech.ac.kr>
2025-01-15 03:26:06 +0900

    Update video_to_lg_r100.sh

--- video_to_lg_r100.sh
+++ video_to_lg_r100.sh
@@ -5,9 +5,14 @@ ENCODER="libx264"
 #vaapi does not work (not sure how to fix this); nvenc works.
 
 #stretch
-ffmpeg -i "$1" -filter:v "transpose=1,scale=608:1080,setsar=1,pad=860:1230:0:75:black" -codec:v $ENCODER -f matroska -y _left.mkv
-ffmpeg -i "$1" -filter:v "transpose=2,scale=608:1080,setsar=1,pad=608:1230:0:75:black" -codec:v $ENCODER -f matroska -y _right.mkv
+#ffmpeg -i "$1" -filter:v "transpose=1,scale=608:1080,setsar=1,pad=734:1230:0:75:black" -codec:v $ENCODER -f matroska -y _left.mkv
+#ffmpeg -i "$1" -filter:v "transpose=2,scale=608:1080,setsar=1,pad=734:1230:126:75:black" -codec:v $ENCODER -f matroska -y _right.mkv
+
+#smaller screen (theatre-like)
+ffmpeg -i "$1" -filter:v "transpose=1,scale=608:1080,setsar=1,pad=1101:1620:0:270:black" -codec:v $ENCODER -f matroska -y _left.mkv
+ffmpeg -i "$1" -filter:v "transpose=2,scale=608:1080,setsar=1,pad=1101:1620:493:270:black" -codec:v $ENCODER -f matroska -y _right.mkv
 
 ffmpeg -i _left.mkv -i _right.mkv -filter_complex "hstack" -codec:v $ENCODER "$1_R100.mkv"
 rm _left.mkv
 rm _right.mkv
+
