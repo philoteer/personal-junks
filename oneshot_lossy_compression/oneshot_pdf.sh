c89e078f7c9dc8e28b4968b86c1c0fe0d5b274d1
philoteer <ksshin@postech.ac.kr>
2025-10-18 21:03:13 +0900

    Update oneshot_pdf.sh

--- oneshot_lossy_compression/oneshot_pdf.sh
+++ oneshot_lossy_compression/oneshot_pdf.sh
@@ -4,6 +4,6 @@ export EXTENSION=pdf
 export TARGET_EXTENSION=pdf
 export EXEC_PATH="runnables/pdfshrink.sh"
 export QUAL_ARG=150 
-export NUM_PROCESS=4
+export NUM_PROCESS=16
 
 bash oneshot_generic.sh
